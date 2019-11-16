644fdb028ee55b222812cd0b0ee754b81fd5da53
hbgames1 <57827813+hbgames1@users.noreply.github.com>
2019-11-16 10:02:54 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("645184787157483544")
+setInterval(function() {
+channel.send(`هلا مبروك هلا مبروك fdiudggdghdg ifdghgjhgfj hfhgdjhgfgjhdg hgdjdhgddg gfdfhjgfgfd hhgjhfdgfdhjfdjhfdjg fhhgfjdhhfdhjfdgjfhd fgdjfhddfjhgdf dhfgjhfdjdgfdjhg jfhdgjhfdfdjhdf fhdjgdjdfgfjdhf fdhdhjdfhgdfhdgdfhg fdhfdgfdjhfddhfdfdg hjdfgjdhfgdfdfjhdf jhdffdjhdfjhdfgfd hdfjjhfdjhfdfdgdfb hfjdfdgfdhfdgfdjdfjh hgjhfdjhfdgdf hdfgsdhfjsjhdfshd hgdfjhdshgdshgdsf gfgdsfhgjdfhgjfd jkhdsgsdhgjfd jgsdfghsdfhgjshsgd sdhgghjdshgjsdgdsh dsghdshgjhgsjsgh dsfhjghgdhgjsfdhjgd sdfhjgdsfghjdhg dhgsjghdhgjd sdfghgjdshgjdshgds dsfhgjdhgjhgjdsfhgdsf rtgiuuigkhgdhjdghjg hffdjhfdhjfdjgjhhgfdjfg fdjhfdjhdgdhg fdjhjfgjfdh fgjhgfkhgndb hrgf dgdjgdfdhfgkjdhfhjfddfj bdfjhfvb nhn b c n nb vnbnb  nbbhfcjfgvjnb ,b fhdvbjv vdhhd bbjhdfb jhdfvuhjfcvhtysdcgbhdjhfgfsdb ffdjhfdb, ,fdvnfdvnb djhgdvfjhdfgfdjkhgseddjsfdhsljigfhlquirehnkfqbuigydrfjhfd qjhhgfjukuyrgfduiyfgqhdgfukqhsbfq qfuyugfjyqyfgjhkfgusqdgfb   bdf dfshjdqfsgfquydfsquqyusbdfqygfoiqsdhfgukqysgdfkqsduhgfkqusyhdfgkqdsdhfhjshdqgfjhgdqgfhgfdfqhdsgffdqgdfqghf  ggdsfqhgfgh qfsا`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
